890c6669faea724e45eefb4cc2fa86c655e85bfe
ChrisCrimson23 <christrabajo98otmail.com>
2023-08-09 22:08:05 -0600

    Primeros componentes

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+
+import './Button.css';
+
+function Button() {
+    return (
+        <button>"Click para ver la magia"</button>
+    );
+}
+
+export default Button;
---
+++ src/Title.js
@@ -0,0 +1,7 @@
+function Title() {
+    return (
+        <h1>Hola desde mi aplicación de REACT</h1>
+    );
+}
+
+export default Title;
